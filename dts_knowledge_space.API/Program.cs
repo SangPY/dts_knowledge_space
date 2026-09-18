627d544aa7fa91e0b4bee8477e93bce0cfda11f9
SangPY <dothanhsang95@gmail.com>
2026-09-18 23:35:09 +0700

    Data Seeding

--- dts_knowledge_space.API/Program.cs
+++ dts_knowledge_space.API/Program.cs
@@ -1,13 +1,43 @@
-using Microsoft.EntityFrameworkCore;
+using System;
 using dts_knowledge_space.API.Data;
+using dts_knowledge_space.API.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+                           .Enrich.FromLogContext()
+                           .WriteTo.Console()
+                           .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog();
 
 // Configure ApplicationDbContext (SQL Server). Ensure a "DefaultConnection" exists in appsettings.json
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Setup Identity
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Services.AddTransient<DbInitializer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +45,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) 
+{ 
+    var services = scope.ServiceProvider; 
+    try 
+    { 
+        Log.Information("Applying migrations and seeding data...");
+        // Ensure database schema is up-to-date before seeding
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        await context.Database.MigrateAsync();
+
+        var dbInitializer = services.GetRequiredService<DbInitializer>();
+        await dbInitializer.Seed();
+    } 
+    catch (Exception ex) 
+    { 
+        var logger = services.GetRequiredService<ILogger<Program>>(); 
+        logger.LogError(ex, "An error occurred while seeding the database."); 
+    } 
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
